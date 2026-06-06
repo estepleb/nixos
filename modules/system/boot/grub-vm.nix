71fb5fb5e5c007f0573abdc192230c8b893e9ee6
Tallis Estevez <tallis.estevez1@gmail.com>
2026-06-06 01:50:54 -0400

    update

--- modules/system/boot/grub-vm.nix
+++ modules/system/boot/grub-vm.nix
@@ -2,10 +2,8 @@
   flake.nixosModules."grub-vm" = { config, lib, pkgs, ... }: {
     boot.loader.grub = {
       enable = true;
-      efiSupport = true;
-      device = "nodev";
+      device = "/dev/sdb";
       useOSProber = true;
     };
-    boot.loader.efi.canTouchEfiVariables = true;
   };
 }
